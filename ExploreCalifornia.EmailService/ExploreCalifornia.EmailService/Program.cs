84b3d5ba08ff17f3df73a26194c6bfbf16370ff5
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-11-23 14:12:28 -0500

    Update Program.cs

--- ExploreCalifornia.EmailService/ExploreCalifornia.EmailService/Program.cs
+++ ExploreCalifornia.EmailService/ExploreCalifornia.EmailService/Program.cs
@@ -1,1 +1,45 @@
 
+using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Collections.Generic;
+
+namespace ExploreCalifornia.EmailService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://emailService:emailServicePassword@localhost:5672");
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("emailServiceQueue", true, false, false);
+            var headers = new Dictionary<string, object>
+            {
+                { "subject","tour"},
+                { "action","booked"},
+                { "x-match","all"}
+            };
+            channel.QueueBind("emailServiceQueue", "webAppExchange", "", headers);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var msg = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+                var subject = Encoding.UTF8.GetString(eventArgs.BasicProperties.Headers["subject"] as byte[]);
+                var action = Encoding.UTF8.GetString(eventArgs.BasicProperties.Headers["action"] as byte[]);
+                Console.WriteLine($"{subject} {action} : {msg}");
+            };
+
+            channel.BasicConsume("emailServiceQueue", true, consumer);
+
+            Console.ReadLine();
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
