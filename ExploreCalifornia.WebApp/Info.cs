fa503a6180662e71d2cd7ba49d23d0ce4a77f68b
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-11-22 20:02:55 -0500

    Update Info.cs

--- ExploreCalifornia.WebApp/Info.cs
+++ ExploreCalifornia.WebApp/Info.cs
@@ -1,5 +1,5 @@
 This is the Booking tours Web App. 
 
-Web application to book fictional tours and it's sending messages to RabbitMQ.
+Web application to book fictional tours and to send confirmation messages to RabbitMQ.
 
 WebApp hase a Headers Exchange to publish messages to RabbitMQ on booking or cancelation of a tour.
