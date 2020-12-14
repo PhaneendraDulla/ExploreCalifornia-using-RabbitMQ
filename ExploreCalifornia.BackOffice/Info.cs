9100d58d9cb536b4f6a9aac76d69aa76851d9d17
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-12-13 21:49:57 -0500

    Create Info.cs

---
+++ ExploreCalifornia.BackOffice/Info.cs
@@ -0,0 +1,5 @@
+This is the Booking tours Back Office App. 
+
+BackOffice hase a backOfficeQueue to receive messages from webAppExchange on booking or cancelling of a tour.
+
+It also declares deadLettersExchange along with this queue.
